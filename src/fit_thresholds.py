e6abb021146bad88e2501f7ffc748add81c182b1
Paul Manis <pmanis@med.unc.edu>
2025-03-26 22:21:42 -0400

    add logic for different fitting function (went back to Hill)

--- src/fit_thresholds.py
+++ src/fit_thresholds.py
@@ -8,38 +8,47 @@ def fit_thresholds(x, y, baseline, threshold_factor=2.5, spl_range=[1, 90]):
     from lmfit import Model
 
     def hill(x, vmax, v50, n):
-        return vmax / (1.0 + (v50 / x) ** n)
+        return vmax / (1.0 + (v50/x) ** n)
+    # def logistic(x, vmax, v50, n):
+    #     return vmax / (1. + np.exp(-(x - v50)/n))
+    from lmfit.models import ExpressionModel
 
+    logistic_model = ExpressionModel("vmax / (1. + exp(-(x - v50)/n))")
+
+    xs = np.linspace(spl_range[0], spl_range[1], sum(spl_range))
+    # if min(x) > 20.:
+    #     zx = np.linspace(0, np.min(x), 20)
+    #     x = np.hstack((zx, x))
+    #     y = np.hstack((np.zeros_like(zx), y))
+    # print(x)
     Hillmodel = Model(hill)
-    Hillmodel.set_param_hint("vmax", min=0.0, max=50.0)
-    Hillmodel.set_param_hint("v50", min=20.0, max=100.0)
+    Hillmodel.set_param_hint("vmax", min=0.0, max=20.0)
+    Hillmodel.set_param_hint("v50", min=30.0, max=100.0)
     Hillmodel.set_param_hint("n", min=0.1, max=50.0)
-
-    params = Hillmodel.make_params(vmax=5, v50=50.0, n=2)
+    params = Hillmodel.make_params(vmax=4, v50=60.0, n=10.0)
     x = np.array(x)
     y = np.array(y)
-    # print("Fitting to: ", x, y, params)
     result = Hillmodel.fit(y, params, x=x, nan_policy='omit')
-    yfit = result.best_fit
-    xs = np.linspace(spl_range[0], spl_range[1], sum(spl_range))
     ys = Hillmodel.eval(x=xs, params=result.params)
 
-    # ithr = np.argwhere(np.array(y) > baseline)
+
+    # logistic_model = Model(logistic)
+    # logistic_model.set_param_hint("vmax", min=0.0, max=20.0)
+    # logistic_model.set_param_hint("n", min=0.1, max=50.0)
+    # logistic_model.set_param_hint("v50", min=20.0, max=100.0)
+    # params = logistic_model.make_params(vmax=4, n=10.0, v50=60.0)
+    # result  = logistic_model.fit(y, x=x, amp=np.max(y), n=5, v50=np.mean(x), nan_policy='omit', method="least_squares") # , weights=1.0/x)
+    # ys = logistic_model.eval(x=xs, params=result.params)
+    # print("result params: ", result.params)
+    # print("np.mean(baseline): ", np.mean(baseline), "data: ", y)
+
     ithr = np.argwhere(np.array(ys) >= np.mean(baseline)*threshold_factor)
     if len(ithr) == 0:
         ithr = len(y) - 1
-        # print("No threshold found: ", row.Subject)
         interp_thr = 100.0
     else:
-        # ithr = ithr[0][0]
-        # m = (y[ithr] - y[ithr - 1]) / (x[ithr] - x[ithr - 1])
-        # b = y[ithr] - m * x[ithr]
-        # bin = 1.0
-        # int_thr = (baseline - b) / m
-        # interp_thr = np.round(int_thr / bin, 0) * bin
-        # print("interp thr: ", row.Subject, int_thr, interp_thr)
         interp_thr = xs[ithr][0][0]
     if interp_thr > 90.0:
         interp_thr = 100.0
-    # print("interpthr: ", interp_thr)
+
     return interp_thr, ithr, (xs, ys)
