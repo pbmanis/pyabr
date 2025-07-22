dd6cf2d986fe050953c356976805322ffc1c43b3
Paul Manis <pmanis@med.unc.edu>
2025-07-22 09:22:09 -0400

    only test fully when running on Windows with hardware.

--- src/test_tdt.py
+++ src/test_tdt.py
@@ -1,16 +1,21 @@
 
-import tdt
-import time
-proj = tdt.DSPProject(interface="USB")
-pa5_1 = tdt.util.connect_pa5(interface="USB", device_id=1)
-pa5_2 = tdt.util.connect_pa5(interface="USB", device_id=1)
-circuit = proj.load_circuit("tdt/abrs_v2.rcx", "RP2")
-circuit.start(0.25)
-proj.trigger('B', 'high')
-time.sleep(1)
-proj.trigger('B', 'low')
 
-pa5_1.SetAtten(40)
-time.sleep(1)
-pa5_1.SetAtten(120)
+import time
+import os
+if os.name != "nt":
+    print("This test is only for Windows, skipping TDT test ")
+else:
+    import tdt
+    proj = tdt.DSPProject(interface="USB")  # type: ignore
+    pa5_1 = tdt.util.connect_pa5(interface="USB", device_id=1)  # type: ignore
+    pa5_2 = tdt.util.connect_pa5(interface="USB", device_id=1)  # type: ignore
+    circuit = proj.load_circuit("tdt/abrs_v2.rcx", "RP2")
+    circuit.start(0.25)
+    proj.trigger('B', 'high')
+    time.sleep(1)
+    proj.trigger('B', 'low')
+
+    pa5_1.SetAtten(40)
+    time.sleep(1)
+    pa5_1.SetAtten(120)
 
--- tests/nidaq_test.py
+++ tests/nidaq_test.py
@@ -1,93 +1,98 @@
 #!/cygdrive/c/Python25/python.exe -i
-## Workaround for symlinks not working in windows
+# Workaround for symlinks not working in windows
 import sys, time, numpy
-# sys.path.append("..\\cheader")
-from nidaq import cheader
-from nidaq import NIDAQ
-import nidaq
-import nidaqmx
-from nidaqmx.constants import AcquisitionType, Edge, VoltageUnits
+import os
+print("os.name: ", os.name)
+if os.name == "posix":
+    print("This test is only for Windows, skipping NIDAQ test ")
+else:
+    # sys.path.append("..\\cheader")
+    from nidaq import cheader
+    from nidaq import NIDAQ
+    import nidaq
+    import nidaqmx
+    from nidaqmx.constants import AcquisitionType, Edge, VoltageUnits
 
-print("Assert num devs > 0:")
-assert(len(NIDAQ.listDevices()) > 0)
-print("  OK")
-print("devices: %s" % NIDAQ.listDevices())
+    print("Assert num devs > 0:")
+    assert(len(NIDAQ.listDevices()) > 0)
+    print("  OK")
+    print("devices: %s" % NIDAQ.listDevices())
 
-print("getDevice:")
-dev0 = NIDAQ.getDevice(b'Dev1')
-print("  ", dev0)
-print(dir(dev0))
-print("\nAnalog Channels:")
-# print "  AI: ", dev0.listAIChannels()
-print("  AO: ", dev0.listAOChannels())
+    print("getDevice:")
+    dev0 = NIDAQ.getDevice(b'Dev1')
+    print("  ", dev0)
+    print(dir(dev0))
+    print("\nAnalog Channels:")
+    # print "  AI: ", dev0.listAIChannels()
+    print("  AO: ", dev0.listAOChannels())
 
-print("\nDigital ports:")
-print("  DI: ", dev0.listDIPorts())
-print("  DO: ", dev0.listDOPorts())
+    print("\nDigital ports:")
+    print("  DI: ", dev0.listDIPorts())
+    print("  DO: ", dev0.listDOPorts())
 
-print("\nDigital lines:")
-print("  DI: ", dev0.listDILines())
-print("  DO: ", dev0.listDOLines())
+    print("\nDigital lines:")
+    print("  DI: ", dev0.listDILines())
+    print("  DO: ", dev0.listDOLines())
 
 def finiteReadTest():
-  task = dev0.createTask()
-  task.CreateAIVoltageChan("/Dev1/ai0", "chan0", nidaq.Val_RSE, -1., 1., nidaq.Val_Volts, None)
-  print('called this function')
-  task.CreateAIVoltageChan("/Dev1/ai1", "chan1", nidaq.Val_Cfg_Default, -10., 10., nidaq.Val_Volts, None)
-  
-  task.CfgSampClkTiming(None, 10000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
-  task.start()
-  data = task.read()
-  task.stop()
-  
-  return data
+    task = dev0.createTask()
+    task.CreateAIVoltageChan("/Dev1/ai0", "chan0", nidaq.Val_RSE, -1., 1., nidaq.Val_Volts, None)
+    print('called this function')
+    task.CreateAIVoltageChan("/Dev1/ai1", "chan1", nidaq.Val_Cfg_Default, -10., 10., nidaq.Val_Volts, None)
+    
+    task.CfgSampClkTiming(None, 10000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
+    task.start()
+    data = task.read()
+    task.stop()
+    
+    return data
 
 
 
 def contReadTest():
-  task = dev0.createTask()
-  task.CreateAIVoltageChan("/Dev1/ai0", "chan0", nidaq.Val_RSE, -10., 10., nidaq.Val_Volts, None)
-  task.CfgSampClkTiming(None, 10000.0, nidaq.Val_Rising, nidaq.Val_ContSamps, 4000)
-  task.start()
-  t = time.time()
-  for i in range(0, 10):
-    data, size = task.read(1000)
-    print("Cont read %d - %d samples, %fsec" % (i, size, time.time() - t))
+    task = dev0.createTask()
+    task.CreateAIVoltageChan("/Dev1/ai0", "chan0", nidaq.Val_RSE, -10., 10., nidaq.Val_Volts, None)
+    task.CfgSampClkTiming(None, 10000.0, nidaq.Val_Rising, nidaq.Val_ContSamps, 4000)
+    task.start()
     t = time.time()
-  task.stop()
+    for i in range(0, 10):
+      data, size = task.read(1000)
+      print("Cont read %d - %d samples, %fsec" % (i, size, time.time() - t))
+      t = time.time()
+    task.stop()
 
 
 ## Output task
 
 def outputTest():
-  with nidaqmx.Task() as task:
-    print(dir(task))
-    #   task = dev0.createTask()
-    # task.CreateAOVoltageChan("/Dev1/ao0", "ao0", -10., 10., nidaq.Val_Volts, None)
-    # print('HERE!!!')
-    task.ao_channels.add_ao_voltage_chan("/dev1/ao0", min_val=-10., max_val=10.)
-    clock = 10000.0
-    duration = 1.0 # seconds
-    ndata = int(duration*clock)
-    freq = 1000.0
-    task.timing.cfg_samp_clk_timing(clock, source="",
-                                    active_edge=Edge.RISING, 
-                                    sample_mode=AcquisitionType.FINITE, samps_per_chan=ndata)
-    t = numpy.arange(0, duration, 1./clock)
-    data = numpy.ones((ndata,), dtype=numpy.float64)*numpy.sin(2.0*numpy.pi*freq*t)
-    import pyqtgraph as pg
-    import sys
-    pg.plot(data)
-    if (sys.flags.interactive != 1) or not hasattr(pg.QtCore, "PYQT_VERSION"):
-          pg.QtGui.QGuiApplication.instance().exec()
+    with nidaqmx.Task() as task:
+      print(dir(task))
+      #   task = dev0.createTask()
+      # task.CreateAOVoltageChan("/Dev1/ao0", "ao0", -10., 10., nidaq.Val_Volts, None)
+      # print('HERE!!!')
+      task.ao_channels.add_ao_voltage_chan("/dev1/ao0", min_val=-10., max_val=10.)
+      clock = 10000.0
+      duration = 1.0 # seconds
+      ndata = int(duration*clock)
+      freq = 1000.0
+      task.timing.cfg_samp_clk_timing(clock, source="",
+                                      active_edge=Edge.RISING, 
+                                      sample_mode=AcquisitionType.FINITE, samps_per_chan=ndata)
+      t = numpy.arange(0, duration, 1./clock)
+      data = numpy.ones((ndata,), dtype=numpy.float64)*numpy.sin(2.0*numpy.pi*freq*t)
+      import pyqtgraph as pg
+      import sys
+      pg.plot(data)
+      if (sys.flags.interactive != 1) or not hasattr(pg.QtCore, "PYQT_VERSION"):
+            pg.QtGui.QGuiApplication.instance().exec()
 
-    print(len(data), data.max(), data.min())
-    # data[200:400] = 5.0
-    # data[600:800] = 5.0
-    task.write(data)
-    task.start()
-    time.sleep(duration)
-    task.stop()
+      print(len(data), data.max(), data.min())
+      # data[200:400] = 5.0
+      # data[600:800] = 5.0
+      task.write(data)
+      task.start()
+      time.sleep(duration)
+      task.stop()
   
 
 
@@ -95,59 +100,60 @@ def outputTest():
 ## Synchronized tasks
 
 def syncADTest():
-  task1 = dev0.createTask()
-  task1.CreateAIVoltageChan("/Dev1/ai0", "ai0", nidaq.Val_Cfg_Default, -10., 10., nidaq.Val_Volts, None)
-  task1.CfgSampClkTiming(None, 10000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
-  task2 = dev0.createTask()
-  task2.CreateDIChan("/Dev1/port0", "di0", nidaq.Val_ChanForAllLines)
-  task2.CfgSampClkTiming("/Dev1/ai/SampleClock", 10000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
+    task1 = dev0.createTask()
+    task1.CreateAIVoltageChan("/Dev1/ai0", "ai0", nidaq.Val_Cfg_Default, -10., 10., nidaq.Val_Volts, None)
+    task1.CfgSampClkTiming(None, 10000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
+    task2 = dev0.createTask()
+    task2.CreateDIChan("/Dev1/port0", "di0", nidaq.Val_ChanForAllLines)
+    task2.CfgSampClkTiming("/Dev1/ai/SampleClock", 10000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
+    
+    data1 = numpy.zeros((1000,), dtype=numpy.float64)
+    data1[200:400] = 5.0
+    data1[600:800] = 5.0
+    task2.start()
+    print("Wrote samples:", task2.write(data1))
+    task1.start()
+    data2 = task1.read()
+    task2.stop()
+    task1.stop()
   
-  data1 = numpy.zeros((1000,), dtype=numpy.float64)
-  data1[200:400] = 5.0
-  data1[600:800] = 5.0
-  task2.start()
-  print("Wrote samples:", task2.write(data1))
-  task1.start()
-  data2 = task1.read()
-  task2.stop()
-  task1.stop()
-  
-  print(data2)
+    print(data2)
 
 
 def syncIOTest():
-  task1 = dev0.createTask()
-  task1.CreateAIVoltageChan("/Dev1/ai0", "", nidaq.Val_RSE, -10., 10., nidaq.Val_Volts, None)
-  task1.CfgSampClkTiming(None, 40000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
+    task1 = dev0.createTask()
+    task1.CreateAIVoltageChan("/Dev1/ai0", "", nidaq.Val_RSE, -10., 10., nidaq.Val_Volts, None)
+    task1.CfgSampClkTiming(None, 40000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
 
-  task2 = dev0.createTask()
-  task2.CreateAOVoltageChan("/Dev1/ao0", "", -10., 10., nidaq.Val_Volts, None)
-  #task2.CfgSampClkTiming(None, 10000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
-  task2.CfgSampClkTiming("ai/SampleClock", 40000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
-  task2.CfgDigEdgeStartTrig("ai/StartTrigger", nidaq.Val_Rising)
-  #task1.SetRefClkSrc("PXI_Clk10")
-  #task2.SetRefClkSrc("PXI_Clk10")
-  print(task1.GetSampClkTimebaseSrc())
-  print(task2.GetSampClkTimebaseSrc())
-  task2.SetSampClkTimebaseSrc("SampleClockTimebase")
-  #task2.SetSyncPulseSrc("/Dev1/SyncPulse")
-
-
-  data1 = numpy.zeros((1000,), dtype=numpy.float64)
-  data1[200:400] = 5.0
-  data1[600:800] = 5.0
-  print("Wrote samples:", task2.write(data1))
-  task2.start()
-  task1.start()
-  data2 = task1.read()
-  #time.sleep(1.0)
-  task1.stop()
-  task2.stop()
-  
-  return data2
+    task2 = dev0.createTask()
+    task2.CreateAOVoltageChan("/Dev1/ao0", "", -10., 10., nidaq.Val_Volts, None)
+    #task2.CfgSampClkTiming(None, 10000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
+    task2.CfgSampClkTiming("ai/SampleClock", 40000.0, nidaq.Val_Rising, nidaq.Val_FiniteSamps, 1000)
+    task2.CfgDigEdgeStartTrig("ai/StartTrigger", nidaq.Val_Rising)
+    #task1.SetRefClkSrc("PXI_Clk10")
+    #task2.SetRefClkSrc("PXI_Clk10")
+    print(task1.GetSampClkTimebaseSrc())
+    print(task2.GetSampClkTimebaseSrc())
+    task2.SetSampClkTimebaseSrc("SampleClockTimebase")
+    #task2.SetSyncPulseSrc("/Dev1/SyncPulse")
 
 
-outputTest()
+    data1 = numpy.zeros((1000,), dtype=numpy.float64)
+    data1[200:400] = 5.0
+    data1[600:800] = 5.0
+    print("Wrote samples:", task2.write(data1))
+    task2.start()
+    task1.start()
+    data2 = task1.read()
+    #time.sleep(1.0)
+    task1.stop()
+    task2.stop()
+    
+    return data2
+
+
+if os.name == 'nt':
+    outputTest()
 
 
 
